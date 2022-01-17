5cff956bf33c7862d9e8a99a367b5fe5965d0c03
andre-bandeli <andre.lbandeli@gmail.com>
2022-01-17 00:07:24 -0300

    consumindo api + tomando erro

--- rest/core/views.py
+++ rest/core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth import logout
 
 
 def home(request):
     return render(request, 'index.html')
+
+def logout_v(request):
+    logout(request)
--- rest/rest/urls.py
+++ rest/rest/urls.py
@@ -5,4 +5,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('accounts/login/', include('allauth.urls')),
+    path('accounts/logout/', include('allauth.urls')),
 ]
