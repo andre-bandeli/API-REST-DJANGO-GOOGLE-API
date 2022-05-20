f97abcb0fd8d398959db8b9d3efdbd8be23dfc1f
andre-bandeli <andre.lbandeli@gmail.com>
2022-05-20 05:15:29 -0300

    feat: novos usuarios

--- rest/core/urls.py
+++ rest/core/urls.py
@@ -2,9 +2,10 @@
 from re import template
 from django.urls import path
 
-from .views import home
+from .views import home, cadastrar_usuario
 
 
 urlpatterns = [
     path('', home, name="index"),
+    path('novouser', cadastrar_usuario, name="register"),
 ]
